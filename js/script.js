940062955d091762a77a7f580b864c0d617fe8bd
anjie_f <amfarmer2022@gmail.com>
2024-11-01 21:26:50 -0500

    Fetching repos

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 const overview = document.querySelector(".overview"); //this targets my profile information
 const username = "Anjie-MF";
+const repoList = document.querySelector(".repo-list");
 
 async function fetchMyInfo() {
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    const resolve = await response.json();
-    displayMyInfo(resolve);
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const resolve = await response.json();
+  displayMyInfo(resolve);
 };
 
 fetchMyInfo();
 
 const displayMyInfo = function (resolve) {
-    const myInfo = document.createElement("div");
-    myInfo.classList.add("user-info");
-    myInfo.innerHTML = `
+  const myInfo = document.createElement("div");
+  myInfo.classList.add("user-info");
+  myInfo.innerHTML = `
     <figure>
     <img alt="user avatar" src=${resolve.avatar_url}/>
   </figure>
@@ -23,5 +24,12 @@ const displayMyInfo = function (resolve) {
     <p><strong>Number of public repos:</strong> ${resolve.public_repos}</p>
   </div>
   `;
-    overview.append(myInfo);
+  overview.append(myInfo);
+};
+
+async function fetchMyRepos() {
+
+  const getRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await getRepos.json();
+  console.log(repoData);
 };
