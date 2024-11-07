84bd9f53d1254aeae22a30f45844fc16f3bf2274
anjie_f <amfarmer2022@gmail.com>
2024-11-07 12:46:22 -0600

    Displayed infro about repos

--- js/script.js
+++ js/script.js
@@ -2,34 +2,44 @@ const overview = document.querySelector(".overview"); //this targets my profile 
 const username = "Anjie-MF";
 const repoList = document.querySelector(".repo-list");
 
-async function fetchMyInfo() {
+
+const fetchMyInfo = async function () {
   const response = await fetch(`https://api.github.com/users/${username}`);
-  const resolve = await response.json();
-  displayMyInfo(resolve);
+  const useMyData = await response.json();
+  displayMyInfo(useMyData);
 };
 
 fetchMyInfo();
 
-const displayMyInfo = function (resolve) {
-  const myInfo = document.createElement("div");
-  myInfo.classList.add("user-info");
-  myInfo.innerHTML = `
+const displayMyInfo = function (useMyData) {
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
     <figure>
-    <img alt="user avatar" src=${resolve.avatar_url}/>
+    <img alt="user avatar" src=${useMyData.avatar_url}/>
   </figure>
   <div>
-    <p><strong>Name:</strong> ${resolve.name}</p>
-    <p><strong>Bio:</strong> ${resolve.bio}</p>
-    <p><strong>Location:</strong> ${resolve.location}</p>
-    <p><strong>Number of public repos:</strong> ${resolve.public_repos}</p>
+    <p><strong>Name:</strong> ${useMyData.name}</p>
+    <p><strong>Bio:</strong> ${useMyData.bio}</p>
+    <p><strong>Location:</strong> ${useMyData.location}</p>
+    <p><strong>Number of public repos:</strong> ${useMyData.public_repos}</p>
   </div>
   `;
-  overview.append(myInfo);
+  overview.append(div);
+  fetchMyRepos();
 };
 
-async function fetchMyRepos() {
-
+const fetchMyRepos = async function () {
   const getRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
   const repoData = await getRepos.json();
-  console.log(repoData);
+  displayReposInfo(repoData);
+};
+
+const displayReposInfo = function (repos) {
+  for (const repo of repos) {
+    const displayRepoItem = document.createElement("li");
+    displayRepoItem.classList.add("repo");
+    displayRepoItem.innerHTML = `<h3>${repo.name}</h3>`;  // Shorter syntax using innerHTML
+    repoList.append(displayRepoItem);  // Appends to the container
+  }
 };
